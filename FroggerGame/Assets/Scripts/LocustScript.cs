1ca26433294746ab5bec5833c8ba6c9c7c9ee94b
Jim Koning <summonjikon@protonmail.com>
2020-06-22 15:24:34 +0200

    FroGearsGame

---
+++ FroggerGame/Assets/Scripts/LocustScript.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+
+public class LocustScript : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+
+    public float minSpeed = 5f; 
+
+    public float maxSpeed = 8f;
+    public float speed = 1f;
+
+    void Start()
+    {
+        speed = Random.Range(maxSpeed, minSpeed);
+    }
+    void FixedUpdate()
+    {
+        Vector2 forward = new Vector2 (transform.right.x, transform.right.y);
+        rb.MovePosition(rb.position + forward * Time.fixedDeltaTime * speed);
+    }
+}
